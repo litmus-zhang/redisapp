15d2a8cd7b1b035c81f291757eae9d9a38b95b88
litmus-zhang <40471908+litmus-zhang@users.noreply.github.com>
2022-05-20 12:30:51 +0100

    car form working fine

---
+++ pages/api/cars.js
@@ -0,0 +1,9 @@
+import { createCar } from "../../lib/redis";
+
+
+export default async function handler(req, res)
+{
+    const id = await createCar(req.body);
+
+    res.status(200).json({ id });
+}
